79a7c9e7ef867d674b169fe3384dd8403b89e973
rinaz <garaevribaz@gmail.com>
2024-12-01 21:56:21 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+from gtts import gTTS
+from tqdm import tqdm
+from rich.prompt import Prompt
+from rich.console import Console
+
+from typing import AnyStr, List
+import csv
+
+URL = "https://edubenchmark.com/blog/environment-ielts-vocabulary/"
+
+
+def req_to_site() -> AnyStr:
+    r = requests.get(URL)
+    if r.status_code == 200:
+        return r.text
+    return ""
+
+
+def parse_words(text: str) -> List[list]:
+    soup = BeautifulSoup(text, "lxml")
+    bodies = soup.find_all(class_="box_heading2")[1:]
+    words = []
+    for body in bodies:
+        ul = body.find(class_="pt_spacer")
+
+        if ul:
+            for li in ul.find_all("li"):
+                words.append(li.text.split(" – "))
+
+    return words
+
+
+def write_to_csv(data: List[list]) -> None:
+    file_name = "words.csv"
+    with open(file_name, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(data)
+    print(f"{file_name} is ready")
+
+
+def create_voice(data: List[list]) -> List[list]:
+    res = []
+    for word in tqdm(data, desc="Creating voice files"):
+        try:
+            tts = gTTS(word[1])
+            tts.save(f"voices/{word[1].replace('/', ' ')}.mp3")
+            res.append([f"[sound:{word[1].replace('/', ' ')}.mp3]", word[0]])
+        except FileNotFoundError:
+            print(word)
+
+    print("Media is ready")
+    return res
+
+
+def main():
+    content = req_to_site()
+    data = parse_words(content)
+    res = create_voice(data)
+    write_to_csv(res)
+
+
+if __name__ == "__main__":
+    console = Console()
+    url = Prompt.ask("[bold green]Please enter the URL[/bold green]:", default="https://")
+    console.print(f"You entered: [bold blue]{url}[/bold blue]")
+    main()
